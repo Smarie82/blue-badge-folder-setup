025f667a0b43306e62c4c8342ba4a4df249fc85c
Stephanie Falls <stephms@gmail.com>
2021-07-29 17:42:25 -0400

    pushing up forked forked code from robs repo first commit hold blue badge module

---
+++ 05-Node-Server/server/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+let express = require('express');
+let app = express();
+const sequelize = require("./db");  
+
+let journal = require("./controllers/journalcontroller");
+let user = require("./controllers/usercontroller");
+
+sequelize.sync();
+
+app.use(express.json());
+
+app.use("/journal", journal);
+app.use("/user", user);
+
+app.listen(3000, function(){
+    console.log("App is listening on port 3000");
+});
